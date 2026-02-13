373d329889781cb197805eb0590be2f830c434c9
Marco Vásquez <marco_vasquez1@unitec.edu>
2026-02-13 09:44:41 -0600

    Clase AdminPalabrasSecretas

--- src/lab/pkg4_juegoahorcado/AdminPalabrasSecretas.java
+++ src/lab/pkg4_juegoahorcado/AdminPalabrasSecretas.java
@@ -11,5 +11,54 @@ import java.util.Random;
  * @author andres
  */
 public class AdminPalabrasSecretas {
+    private ArrayList<String> palabras;
+    private Random random;
     
+    public void agregarPalabra(String palabra) throws PalabraDuplicadaException{
+        String palabraMinuscula=palabra.toLowerCase();
+        if(palabras.contains(palabraMinuscula)){
+            throw new PalabraDuplicadaException("La palabra '"+palabra+"' ya existe en la lista");
+        }
+    }
+    public String obtenerPalabraRandm() throws ListaVaciaException{
+        if(palabras.isEmpty()){
+            throw new ListaVaciaException("No hay palabras disponibles");
+        }
+        int indice;
+        indice=random.nextInt(palabras.size());
+        return palabras.get(indice);
+    }
+    public ArrayList<String> obtenerPalabras(){
+        return new ArrayList<>(palabras);
+    }
+    public int getCantidadPalabras(){
+        return palabras.size();
+    }
+    private void cargarPalabras(){
+        try{
+            agregarPalabra("programacion");
+            agregarPalabra("computadora");
+            agregarPalabra("Netbeans");
+            agregarPalabra("internet");
+            agregarPalabra("tecnologia");
+            agregarPalabra("frijoles");
+            agregarPalabra("sueter");
+            agregarPalabra("destornillador");
+            agregarPalabra("supercalifragilisticoespialidoso");
+            agregarPalabra("aplicacion");
+            agregarPalabra("internet");
+            agregarPalabra("software y hardware");
+            agregarPalabra("paginas web");
+            agregarPalabra("configuraciones");
+            
+        }
+        catch(PalabraDuplicadaException e){
+            System.out.println("Error en cargar las palabras: "+e.getMessage());
+        }
+    }
+    public AdminPalabrasSecretas(){
+        palabras=new ArrayList<>();
+        random=new Random();
+        cargarPalabras();
+    }
 }
